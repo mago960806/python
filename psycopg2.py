d95a6c4568f5802a0b6f6a508221b704f0c13d6b
mago960806 <mago960806@hotmail.com>
2017-10-29 14:45:00 +0800

    Delete psycopg2.py

--- psycopg2.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-conn=psycopg2.connect(database="venustech",user="venustech",password="sysadmin@028",host="134.224.126.3",port="5432")
-cursor=conn.cursor()
-cursor.execute("SELECT name,password FROM t_acc_master where name='sxy802102';")
-result=cursor.fetchone()
-print(result[0],"的密码为:",result[1])
-cursor.close()
-conn.close()
